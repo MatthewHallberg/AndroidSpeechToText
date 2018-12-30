4749b2dc1e893c310f0d451460d8d815f7edbb6b
Matthew Hallberg <matthewhallberg@gmail.com>
2018-12-30 13:56:31 -0500

    first commit

---
+++ Assets/Scripts/VoiceController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceController : MonoBehaviour {
+
+
+
+    AndroidJavaObject activity;
+    AndroidJavaObject plugin;
+
+    private void Start() {
+        InitPlugin();
+    }
+
+    void InitPlugin() {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+
+        activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+                plugin = new AndroidJavaObject(
+                "com.example.matthew.plugin.VoiceBridge");
+        }));
+
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+            plugin.Call("StartPlugin");
+        }));
+    }
+
+    /// <summary>
+    /// gets called via SendMessage from the android plugin GameObject must be called "VoiceController"
+    /// </summary>
+    /// <param name="recognizedText">recognizedText.</param>
+    public void OnVoiceResult(string recognizedText) {
+        Debug.Log(recognizedText);
+
+    }
+
+    /// <summary>
+    /// gets called via SendMessage from the android plugin
+    /// </summary>
+    /// <param name="error">Error.</param>
+    public void OnErrorResult(string error) {
+        Debug.Log(error);
+
+    }
+
+    public void GetSpeech() {
+        // Calls the function from the jar file
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+            plugin.Call("StartSpeaking");
+        }));
+    }
+}
